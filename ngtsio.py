6220b9c488c679fa3ff5899284f22139861e0775
cjduffy <c.jduffy@me.com>
2021-09-22 15:23:55 +0100

    Changed imports to work globally in ngtsio.py

--- ngtsio.py
+++ ngtsio.py
@@ -13,8 +13,8 @@ Email: mg719@cam.ac.uk
 
 
 import numpy as np
-import ngtsio_find
-import ngtsio_get
+from ngtsio import ngtsio_find
+from ngtsio import ngtsio_get
 import pickle
 
 
@@ -24,7 +24,7 @@ import pickle
 ###############################################################################
 __version__ = '1.5.6'
 
-    
+
 '''
 Note that cfitsio/fitsio is 20x faster than astropy/pyfits for single objects
 pyfits (all objects, 5x) 29.4824950695s
@@ -41,23 +41,23 @@ fitsio (one object, 5x)  0.49312210083s
 def find(RA, DEC, ngts_version='all', unit='hmsdms', frame='icrs',
          give_obj_id=True, search_radius=0.0014, field_radius=2., outfname=None):
     '''find the obj_id of a given RA and Dec with ngtsio_find.py; see ngtsio_find.py for docstring'''
-    
+
     print('#RA\tDEC\tfieldname\tngts_version\tobj_id')
-    ngtsio_find.find(RA, DEC, ngts_version=ngts_version, unit=unit, frame=frame,  
-                     give_obj_id=give_obj_id, search_radius=search_radius, 
+    ngtsio_find.find(RA, DEC, ngts_version=ngts_version, unit=unit, frame=frame,
+                     give_obj_id=give_obj_id, search_radius=search_radius,
                      field_radius=field_radius, outfname=outfname)
-    
-    
-    
+
+
+
 def find_list(fname, usecols=(0,1), ngts_version='all', unit='hmsdms', frame='icrs',
               give_obj_id=True, search_radius=0.014, field_radius=2., outfname=None):
     '''find the obj_id of multiple given RAs and Decs with ngtsio_find.py; see ngtsio_find.py for docstring'''
-    
+
     print('#RA\tDEC\tfieldname\tngts_version\tobj_id')
     RAs, DECs = np.genfromtxt(fname, usecols=usecols, delimiter='\t', dtype=None, unpack=True)
     for i in range(len(RAs)):
-        ngtsio_find.find(RAs[i], DECs[i], ngts_version=ngts_version, unit=unit, frame=frame, 
-                     give_obj_id=give_obj_id, search_radius=search_radius, 
+        ngtsio_find.find(RAs[i], DECs[i], ngts_version=ngts_version, unit=unit, frame=frame,
+                     give_obj_id=give_obj_id, search_radius=search_radius,
                      field_radius=field_radius, outfname=outfname)
 
 
@@ -67,38 +67,38 @@ def find_list(fname, usecols=(0,1), ngts_version='all', unit='hmsdms', frame='ic
 # Getter (Main Program)
 ###############################################################################
 
-def get(fieldname, ngts_version, keys, obj_id=None, obj_row=None, 
-        time_index=None, time_date=None, time_hjd=None, time_actionid=None, 
-        bls_rank=1, indexing='fits', fitsreader='fitsio', simplify=True, 
+def get(fieldname, ngts_version, keys, obj_id=None, obj_row=None,
+        time_index=None, time_date=None, time_hjd=None, time_actionid=None,
+        bls_rank=1, indexing='fits', fitsreader='fitsio', simplify=True,
         fnames=None, root=None, roots=None, silent=False, set_nan=False):
     '''get data for a given object with ngtsio_get.py; see ngtsio_get.py for docstring'''
-            
-    dic = ngtsio_get.get(fieldname, ngts_version, keys, obj_id=obj_id, obj_row=obj_row, 
-        time_index=time_index, time_date=time_date, time_hjd=time_hjd, time_actionid=time_actionid, 
-        bls_rank=bls_rank, indexing=indexing, fitsreader=fitsreader, simplify=simplify, 
+
+    dic = ngtsio_get.get(fieldname, ngts_version, keys, obj_id=obj_id, obj_row=obj_row,
+        time_index=time_index, time_date=time_date, time_hjd=time_hjd, time_actionid=time_actionid,
+        bls_rank=bls_rank, indexing=indexing, fitsreader=fitsreader, simplify=simplify,
         fnames=fnames, root=root, roots=roots, silent=silent, set_nan=set_nan)
-            
+
     return dic
-    
-    
-    
+
+
+
 
 ###############################################################################
 # Save to pickle
 ###############################################################################
-    
-def save(outfilename, fieldname, ngts_version, keys, obj_id=None, obj_row=None, 
-        time_index=None, time_date=None, time_hjd=None, time_actionid=None, 
-        bls_rank=1, indexing='fits', fitsreader='fitsio', simplify=True, 
+
+def save(outfilename, fieldname, ngts_version, keys, obj_id=None, obj_row=None,
+        time_index=None, time_date=None, time_hjd=None, time_actionid=None,
+        bls_rank=1, indexing='fits', fitsreader='fitsio', simplify=True,
         fnames=None, root=None, roots=None, silent=False, set_nan=False):
     '''save data for a given object to outfilename.pickle via ngtsio_get.py; see ngtsio_get.py for docstring'''
-            
-    dic = ngtsio_get.get(fieldname, ngts_version, keys, obj_id=obj_id, obj_row=obj_row, 
-        time_index=time_index, time_date=time_date, time_hjd=time_hjd, time_actionid=time_actionid, 
-        bls_rank=bls_rank, indexing=indexing, fitsreader=fitsreader, simplify=simplify, 
+
+    dic = ngtsio_get.get(fieldname, ngts_version, keys, obj_id=obj_id, obj_row=obj_row,
+        time_index=time_index, time_date=time_date, time_hjd=time_hjd, time_actionid=time_actionid,
+        bls_rank=bls_rank, indexing=indexing, fitsreader=fitsreader, simplify=simplify,
         fnames=fnames, root=root, roots=roots, silent=silent, set_nan=set_nan)
-        
+
     pickle.dump( dic, open( outfilename+'.pickle', 'wb' ) )
-    
-    
- 
+
+
+
